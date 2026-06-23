d53e10b75e85810284622d1559b6d367ec5d44ae
Justin <perezjustin0715@gmail.com>
2026-06-23 16:17:49 +0800

    chore: cleanup redundant root backend files and test scripts

--- services/db.py
+++
@@ -1,4 +0,0 @@
-﻿from supabase import create_client, Client
-from config import SUPABASE_URL, SUPABASE_KEY
-
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY) if SUPABASE_URL and SUPABASE_KEY else None
